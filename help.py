133e2605aada3df86986436ec50e5458a5d18626
mihai cziraki <58424830+czirakim@users.noreply.github.com>
2021-03-17 15:44:51 +0200

    Create  help.py

---
+++ help.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+
+class help:
+
+  def __init__(self, channel):
+    self.channel = channel
+
+  def _help(self):
+    z = "Supported commands: inventory , interfaces status <device>, interfaces down, wifi clients, PiTemp"
+    text = f"{z}"
+    return {"type": "section", "text": {"type": "mrkdwn", "text": text}},
+
+  def get_message_payload(self):
+        return {
+            "channel": self.channel,
+            "blocks": [
+                *self._help(),
+            ],
+        }
