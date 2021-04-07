3a8ce097cc064ab4ba386f20dc8af1032e0eb7f1
mihai cziraki <58424830+czirakim@users.noreply.github.com>
2021-04-07 11:33:40 +0300

    Update help.py

--- help.py
+++ help.py
@@ -8,7 +8,7 @@ class help:
     self.channel = channel
 
   def _help(self):
-    z = "Supported commands: inventory , interfaces status <device>, interfaces down, wifi clients, PiTemp"
+    z = "Supported commands: inventory , interfaces status <device>, interfaces down, wifi clients, PiTemp, myip, whois <ip>"
     text = f"{z}"
     return {"type": "section", "text": {"type": "mrkdwn", "text": text}},
 
